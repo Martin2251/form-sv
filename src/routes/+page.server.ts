d0e9f07cf74870991bfc4fe46574730806d2a64d
Martin Lowe <martinlowe673@gmail.com>
2024-11-10 19:18:25 +0000

    password

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,8 @@ import {z} from 'zod'
 
 const registerSchema = z.object({
     name: z.string().min(1).max(64).trim(),
-    email: z.string().min.max(64).email()
+    email: z.string().min(1).max(64).email(),
+    password:z.string().min(6).max(12),
 })
 
 
