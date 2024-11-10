c390211a3eb93dfb1ec1a80764be98dc5bba861e
Martin Lowe <martinlowe673@gmail.com>
2024-11-10 19:06:52 +0000

    form object

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,13 @@
+import {z} from 'zod'
+
+
+const registerSchema = z.object({
+    name: z.string().min(1).max(64).trim(),
+    email: z.string().min.max(64).email()
+})
+
+
+
 export const actions = {
 
     default: async ({request}:any) =>{
